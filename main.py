2906a215f4c0247e3f55aff965bc537d18793c8f
nuxion <nuxion@gmail.com>
2021-07-28 15:12:46 -0300

    Correcciones

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ print("Bienvenido a Casino Royale")
 print("**************************")
 
 #Variables globales
+# No es una buena practica usar variables globales.
+# Hace dificil ademas razonar sobre el codigo.
 ganaste = 0       #fichas
 perdiste = 0      #fichas
 _ganaste = 0      #noches
@@ -19,8 +21,15 @@ total_jugadas = 0 #apuestas totales
 #Funcion
 def apuestas(fichas , probabilidad , maximo_jugado):
     tmp = 0
+    # cuidado tambien con la combinacion ingles/español para la nomemclatura.
+    # idealmente uno u otro.
     winrate = 0
     lossrate = 0
+    # cuidado con el nombre de variables:
+    # maximo_jugado podria entenderse como la
+    # cantidad de veces que ya se juegom en cambio es un
+    # cantidad_de_veces_a_jugar, o simplemente "a_jugar" o "maximo_juegos",
+    # "max_juegos"
     while fichas > 0 and tmp < maximo_jugado:
         lista = [0,1]
         resultado = random.choices(lista,weights=[1-probabilidad,probabilidad])
@@ -36,6 +45,9 @@ def apuestas(fichas , probabilidad , maximo_jugado):
         tmp += 1
     if fichas == 0:
         contador =- 1
+        # Puede ser complicado trabajar con resultados de listas:
+        # a largo plazo requiere volver sobre la funcion para entender
+        # en que orden se mandaron ciertas variables. 
         return(f"Perdiste, te quedan {fichas} fichas y apostaste {tmp} veces", contador , winrate , lossrate)
     if fichas > 0:
         contador = 1
@@ -50,8 +62,11 @@ for _ in range(1,21):
     #Llama la funcion y crea una tupla con todos los datos a utilizar
     datos = apuestas(50 , 0.4 , 300)#fichas, probabilidad, maximo jugado
     print(f"{datos[0]}")
+
     
     #Calculo de "apuestas" ganadas por "noche"
+    # Esta logica podria estar dentro de la funcion apuestas ,
+    # y que el for ... range() sea simplemente para iterar las simulaciones/jugadas.
     ganaste = datos[2]
     perdiste = datos[3]
     jugadas = ganaste + perdiste
@@ -70,6 +85,7 @@ for _ in range(1,21):
 print("\n******************************************")
 print(f"* Ganaste {_ganaste} noches y perdiste {_perdiste} noches. *")
 print(f"* Apostaste {total_jugadas} veces.                  *")
+# Bien la funcion de redondeo.
 print(f"* Un promedio de {total_jugadas/20:.2f} por noche.       *")
 print("******************************************\n")
     
